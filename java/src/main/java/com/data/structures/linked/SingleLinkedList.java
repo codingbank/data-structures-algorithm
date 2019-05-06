a273759fb23b3855fe1324d3c2013bc2011c1cb3
王佳辉 <wangjiahui@edianzu.com>
2019-05-06 15:20:00 +0800

    add single linked list class

---
+++ java/src/main/java/com/data/structures/linked/SingleLinkedList.java
@@ -0,0 +1,11 @@
+package com.data.structures.linked;
+
+/**
+ * @author [wangjiahui]
+ * @ClassName: SingleLinkedList
+ * @CreateDate: [2019-05-06 15:19]
+ * @Description: [TODO]
+ * @version: [V1.0]
+ */
+public class SingleLinkedList {
+}
